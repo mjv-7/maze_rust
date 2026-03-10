9f380424e79f116a00bf98554ee97974fe59625a
mjv-7 <mujibullafikrat@gmail.com>
2026-03-10 09:45:00 -0400

    update 5

--- src/screen2.rs
+++ src/screen2.rs
@@ -21,6 +21,16 @@ fn draw_grid_standard(grid_size: f32, color: Color) {
 }
 
 pub async fn run() -> String {
+    let mut wall = StillImage::new(
+        "assets/blackscreen.png",
+        180.0, // width
+        15.0,  // height
+        550.0, // x position
+        900.0, // y position
+        true,  // Enable stretching
+        1.0,   // Zoom level (1.0 = 100%)
+    )
+    .await;
     let img = StillImage::new(
         "assets/maze.png",
         1440.0, // width
@@ -31,7 +41,7 @@ pub async fn run() -> String {
         1.0,    // Zoom level (1.0 = 100%)
     )
     .await;
-    let door_img = StillImage::new(
+    let mut door_img = StillImage::new(
         "assets/door.png",
         50.0,  // width
         180.0, // height
@@ -64,41 +74,56 @@ pub async fn run() -> String {
     let mut door_exists = true;
     let mut key = false;
     let mut player = Player::new("assets/mario.png".to_string(), 350.0, 50.0, 50.0, 50.0, 50.0).await;
-    // let player_keys = player::Player::new("assets/mario_keys.png".to_string(), 200.0, 50.0, 50.0, 50.0, 50.0).await;
     loop {
         clear_background(WHITE);
+        img.draw();
+        keys.draw();
+        player.draw();
+        end.draw();
+        wall.draw();
+        if door_exists {
+            door_img.draw();
+        }
+        draw_grid(50.0, BLACK);
         player.key_press();
         if player.collision(&keys) {
             player.set_texture("assets/mario_keys.png").await;
             key = true;
             keys.clear();
+            println!("key achieved");
         }
-        if key && door_exists && player.collision(&door_img) {
-            door_exists = false;
-        }
-
-        if door_exists {
-            door_img.draw();
-        }
-
+        // println!("key: {}", key);
+        //println!("door_exists: {}", door_exists);
+        // println!("player.collision(&door_img): {}", player.collision(&door_img));
+      
         // Save old position in case of collision
-        if player.collision_x(&img) || player.collision_x(&door_img) {
+        if player.collision_x(&img) {
             player.back_x();
         }
-        if player.collision_y(&img) || player.collision_y(&door_img) {
+        if player.collision_y(&img) {
             player.back_y();
         }
-
+        if player.collision(&end) {
+            return "screen1".to_string();
+        }
+        if door_exists {
+            if player.collision(&door_img) {
+                if key {
+                    door_exists = false;
+                    println!("door destroyed");
+                    door_img.clear();
+                    player.set_texture("assets/mario.png").await;
+                } else {
+                    player.back_x();
+                    player.back_y();
+                }
+            }
+        }
         draw_text("Screen 2", 20.0, 40.0, 30.0, WHITE);
 
         if is_key_pressed(KeyCode::Space) {
             return "screen1".to_string();
         }
-        img.draw();
-        keys.draw();
-        player.draw();
-        end.draw();
-        draw_grid(50.0, BLACK);
         next_frame().await;
     }
 }
