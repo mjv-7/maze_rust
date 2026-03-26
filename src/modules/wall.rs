219e973bd9df31b23145223c46c6a7a1bb72000e
mjv-7 <mujibullafikrat@gmail.com>
2026-03-26 11:07:45 -0400

    update

--- src/modules/wall.rs
+++ src/modules/wall.rs
@@ -1,16 +1,27 @@
+/*
+ pub mod wall
+*/ 
 use macroquad::prelude::*;
 use crate::modules::still_image::{self, StillImage};
+use crate::modules::collision::check_collision;
 
 
 pub struct Wall {
-    pub view: StillImage,
-    pub move_speed: f32,
+    view: StillImage,
+    move_speed: f32,
     movement: Vec2,
 }
 
 impl Wall {
 
-    pub async fn new(asset_path: String, move_speed: f32,  width: f32, height: f32, x: f32, y: f32,) -> Self {
+    pub async fn new(asset_path: String, 
+        move_speed: f32,  
+        width: f32, 
+        height: f32, 
+        x: f32, 
+        y: f32,
+        stretch_enabled:bool,
+        zoom_level: f32) -> Self {
         Wall {
             view: StillImage::new(&asset_path, 50.0, 50.0, x, y, true, 1.0).await,
             move_speed: move_speed,
@@ -18,7 +29,7 @@ impl Wall {
         }
     }
     pub fn moving(&mut self, img_out: &StillImage) -> Self {
-        if img_out.collision() {
+        if img_out.collision {
 
         }
             
